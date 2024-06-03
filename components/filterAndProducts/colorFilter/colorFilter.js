c4886b29e54b05bcaf9098bb940b643c15cc66c4
Marko <markov991@gmail.com>
2024-06-03 23:17:35 +0200

    global state filters

--- components/filterAndProducts/colorFilter/colorFilter.js
+++ components/filterAndProducts/colorFilter/colorFilter.js
@@ -1,8 +1,31 @@
 import { useState, useEffect } from "react";
 import classes from "./colorFilter.module.css";
+import { filterSliceActions } from "@/store/filtersSlice";
+import { useDispatch } from "react-redux";
 
 export default function ColorFilter({ colors }) {
+  const dispatch = useDispatch();
   const [expended, setExpended] = useState(false);
+
+  const [checkedColors, setCheckedColors] = useState([]);
+
+  const handleCheckboxChange = (event) => {
+    const { name, checked } = event.target;
+    console.log(name, checked);
+    if (checked) {
+      setCheckedColors([...checkedColors, name]);
+    }
+    if (!checked) {
+      setCheckedColors(checkedColors.filter((colors) => colors !== name));
+    }
+  };
+
+  useEffect(() => {
+    dispatch(
+      filterSliceActions.changeColorFilterParams({ pickedColor: checkedColors })
+    );
+    console.log(checkedColors);
+  }, [checkedColors]);
 
   return (
     <div>
@@ -16,7 +39,12 @@ export default function ColorFilter({ colors }) {
         <ul className={classes.listStyling}>
           {colors.map((color) => (
             <li key={color}>
-              <input name={color} id={color} type="checkbox" />
+              <input
+                name={color}
+                id={color}
+                type="checkbox"
+                onChange={handleCheckboxChange}
+              />
               <label htmlFor={color}>{color}</label>
             </li>
           ))}
