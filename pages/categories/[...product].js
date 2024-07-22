5d3b218db59221050eb89bb235b157affa153bc4
Marko <markov991@gmail.com>
2024-07-22 22:16:24 +0200

    staticProps function finished

--- pages/categories/[...product].js
+++ pages/categories/[...product].js
@@ -1,48 +1,36 @@
 import React from "react";
 import { connectToDatabase, getProductDetails, getAllPaths } from "@/lib/db";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
 
 export default function ProductPage(props) {
-  const router = useRouter();
+  const {
+    name,
+    category,
+    description,
+    average_rating,
+    brand,
+    color,
+    images,
+    productId,
+    selling_price,
+    reviews_count,
+    availability,
+  } = props.productDetails;
 
-  const productName = router.query.product;
-  const [productDetails, setProductDetails] = useState({});
   console.log(props);
-  useEffect(() => {
-    if (productName[1]) {
-      fetch(`/api/getSpecificProduct?productId=${productName[1]}`)
-        .then((response) => response.json())
-        .then((data) => {
-          setProductDetails({ ...data.product });
-        });
-    }
-  }, []);
 
-  useEffect(() => {
-    console.log(productDetails);
-    console.log(props.productDetails);
-  }, [productDetails]);
-
-  if (!productName) {
-    return <div>LOADING</div>;
-  }
-  if (productName) {
-    return (
-      <>
-        <div>This is product {productName[1]} </div>
-        <div>{props.selling_price}</div>
-      </>
-    );
-  }
+  return (
+    <>
+      <div>{name}</div>
+    </>
+  );
 }
 
 export async function getStaticProps(context) {
   const productId = context.params.product[1];
   const client = await connectToDatabase();
-  const productDetails = await getProductDetails(client, productId);
+  const productDetails = getProductDetails(client, productId);
   const { _id, ...data } = await productDetails;
-  console.log("THESE ARE GET STATIC PROPS:", productDetails);
+
   return {
     props: {
       productDetails: data,
